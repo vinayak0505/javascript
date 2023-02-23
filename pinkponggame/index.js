3087a91e22f96cc569624ca3cf035ee80f86f87c
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-02-23 15:04:43 +0530

    added a ball

--- pinkponggame/index.js
+++ pinkponggame/index.js
@@ -1,4 +1,38 @@
 let slates = document.getElementsByClassName("slate");
+let ball = document.getElementById("ball");
+
+
+let isRed = true;
+setInterval(changeBallColor, 1000);
+function changeBallColor() {
+    if(isRed) ball.style.backgroundColor = "rgb(122, 0, 0)";
+    else ball.style.backgroundColor = "red";
+    isRed = !isRed;
+}
+
+let horizontal = 1;
+let vertical = 0;
+setInterval(changeBall, 1);
+function changeBall() {
+    ball.style.left = (horizontal + ball.getBoundingClientRect().left) + 'px';
+    ball.style.top = (vertical + ball.getBoundingClientRect().top) + 'px';
+    checkBallRight();
+    if(vertical > 0){
+        ball.getBoundingClientRect().left
+    }
+}
+
+function checkBallRight() {
+    console.log(ball.getBoundingClientRect().right);
+    console.log(screen.width);
+    if(ball.getBoundingClientRect().right >= screen.width && horizontal > 0){
+        horizontal = -horizontal;
+    }
+    
+    if(ball.getBoundingClientRect().left <= 0 && horizontal < 0){
+        horizontal = -horizontal;
+    }
+}
 
 function move(by) {
     for (var i = 0;i < slates.length; i++) {
@@ -6,7 +40,7 @@ function move(by) {
     }
 }
 
-const SPEED = 10;
+const SPEED = 50;
 document.addEventListener('keydown',
     function (event) {
         switch (event.key) {
